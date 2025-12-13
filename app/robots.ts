00a9560ae6a530e0ee6e21adc7c5663390c17308
AnshulVatsOfficial <avanshul0001@gmail.com>
2025-12-13 18:34:04 +0530

    fix: create ads.txt file in public folder

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: ["/", "/ads.txt"],
       },
     ],
     sitemap: "https://unitlab.org/sitemap.xml",
